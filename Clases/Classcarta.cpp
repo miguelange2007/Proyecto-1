b6e06493491dd3f945fff0cf24dec90257ab4b7d
miguelange2007 <miguelange2007@gmail.com>
2026-04-04 18:59:37 -0500

    Add Carta class with constructors for valor

---
+++ Clases/Classcarta.cpp
@@ -0,0 +1,18 @@
+#ifndef ClassCarta_H
+#define ClassCarta_H
+class Carta{
+
+    public:
+
+        int valor;
+
+        Carta(){
+            valor = 0;
+        }
+
+        Carta(int v){
+            valor = v;
+        }
+
+};
+#endif
